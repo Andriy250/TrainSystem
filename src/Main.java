83221eea4e3f99adec734b196b85679c920b130a
YaroslavKovalchuk <35632982+YaroslavKovalchuk@users.noreply.github.com>
2018-12-06 22:06:18 +0200

    add classes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 public class Main {
@@ -6,6 +8,24 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Date time = new Date();
-        System.out.println(time);
+
+        Train train = new Train();
+        Wagon wagon = new Wagon();
+        Place place = new Place(train,wagon,"CUPE",220);
+        Station station = new Station("Lviv");
+        TrainSchedule trainSchedule = new TrainSchedule(station,new Date(time.getTime()),new Date(time.getTime()));
+
+        Customer customerVova = new Customer("Vova");
+
+        List<Customer>  customers = new ArrayList<>();
+        List<Place> places = new ArrayList<>();
+        List<TrainSchedule> trainSchedules = new ArrayList<>();
+        customers.add(customerVova);
+        places.add(place);
+        trainSchedules.add(trainSchedule);
+
+        Trip trip = new Trip(new Route(train,trainSchedules),customers,places);
+        System.out.println(trip);
+
     }
 }
